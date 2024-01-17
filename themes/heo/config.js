dd07b523830369df6cb58307d6fe67ef61a93cd0
Vtea <3939@live.com>
2024-01-17 15:35:56 +0800

    edit vtea info

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '中国可用Chatgpt站1', url: 'https://u.aiprm.top' },
+    { title: '中国可用Chatgpt站2', url: 'https://chat.mice.pub' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '掌握人工智能',
+  HEO_HERO_TITLE_2: '领跑未来职场',
+  HEO_HERO_TITLE_3: 'vtea.ltd',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '新一代AI自动内容创作平台',
+  HEO_HERO_TITLE_LINK: 'https://u.aiprm.top',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -37,7 +37,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://w.cn2.ltd', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -103,7 +103,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=p8ZJ8Em38i0HIZudvWc0kxUpUsDKnHH-&authKey=vMjaBKuLVp9ag7KkvtiQXo0i9kjNIG22PfgDPMR7huPwQ%2FkOu31%2B4WYZuACSXxPV&noverify=0&group_code=181509991',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
